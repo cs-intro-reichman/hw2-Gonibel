a910868fa35c0437523c98354901085043689fd1
Gonibel <gbelkine@gmail.com>
2024-11-15 20:19:51 +0200

    Update CalcPi.java

--- CalcPi.java
+++ CalcPi.java
@@ -5,20 +5,15 @@ public class CalcPi {
 
 		String laps1 = args[0];
 		int laps = Integer.parseInt(laps1);
-		double sum = 1.0;
-		int counter = 1;
+		double sum = 0.0;
+		double term;
 
-		for( int i = 3; i < laps; i = i + 2) {
-			if (counter % 2 == 1) {
-				sum -= (1.0/i);
-			} else {
-				sum += (1.0/i);
-			}
-			counter++;
+		for (int i = 0; i < laps; i++) {
+			term = (1.0/(i * 2 + 1));
+			if (i % 2 == 0){ sum += term;}
+			else { sum -= term;}
 		}
-		
 		sum = sum * 4;
-
 		System.out.println("pi, approximated:     " + Math.PI);
 		System.out.println("pi according to Java:     " + sum);
 
